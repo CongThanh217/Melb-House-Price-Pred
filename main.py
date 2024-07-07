d8a0ecf8420d2719c9809328cca1b470d8db4767
CongThanh217 <congthanhhis@gmail.com>
2024-07-07 10:31:10 +0700

    data_eda

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+import numpy as np
 import pandas as pd
+from sklearn.ensemble import ExtraTreesRegressor
 from ydata_profiling import ProfileReport
+from sklearn.impute import KNNImputer
+from sklearn.experimental import enable_iterative_imputer
+from fancyimpute import IterativeImputer
+from missforest.missforest import MissForest
+
 
 df = pd.read_csv("melb_data.csv")
+# Parsing the Address column (get name of street)
+df["Address"] = df["Address"].apply(lambda x: x.split(" ")[1])
 
-report = ProfileReport(df, title="melb_house_data")
+# Get the year of Date
+df["Date"] = df["Date"].apply(lambda x: x.split("/")[2])
 
-report.to_file("melb_data.html")
+# Handle nan in CouncilArea column
+df['CouncilArea'].fillna('Unknown', inplace=True)
+
+# Drop nan rows
+df = df.dropna(axis=0)
+df["Suburb"].value_counts()
+df.to_csv("Data_clean.csv")
+# BuildingArea
+# YearBuilt
+# CouncilArea
+
